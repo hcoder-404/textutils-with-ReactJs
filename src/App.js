073ed5342b9ae3aaea91e82ece3fff8b2b203671
Hcoder <harshchauhan1912@gmail.com>
2025-06-19 22:14:40 +0530

    Add New...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import  React, { useState } from 'react'; 
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
+
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 let name = "Harsh";
 function App() {
@@ -44,14 +51,17 @@ function App() {
   }
   return (
     <>  
+    <Router>
     <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert} />  
 
     <div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter the text to analyze below"  mode={mode} />
-    {/* <About /> */}   
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below"  mode={mode} />} />
+      </Routes>
     </div> 
-
+    </Router> 
     </>  
   ); 
 }
